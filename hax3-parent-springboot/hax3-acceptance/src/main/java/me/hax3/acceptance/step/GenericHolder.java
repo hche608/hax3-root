a5a29df5e3a9941efac14c74fbe54cc0e2875a1c
Hao Chen <hao.chen@vector.co.nz>
2018-06-28 20:40:17 +1200

    clean up GenericHolder

--- hax3-parent-springboot/hax3-acceptance/src/main/java/me/hax3/acceptance/step/GenericHolder.java
+++ hax3-parent-springboot/hax3-acceptance/src/main/java/me/hax3/acceptance/step/GenericHolder.java
@@ -1,10 +1,5 @@
 package me.hax3.acceptance.step;
 
-import org.springframework.context.annotation.Scope;
-import org.springframework.stereotype.Component;
-
-@Component
-@Scope("cucumber-glue")
 public class GenericHolder<T> {
 
     private T value;
