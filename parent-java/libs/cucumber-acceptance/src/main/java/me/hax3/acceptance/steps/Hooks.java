3fcc0fa5310dfe080d7a89dcc8cf38f62b994cf9
Hao Chen <ilff8chen@gmail.com>
2019-09-16 18:56:30 +1200

    testing new Hooks

--- parent-java/libs/cucumber-acceptance/src/main/java/me/hax3/acceptance/steps/Hooks.java
+++ parent-java/libs/cucumber-acceptance/src/main/java/me/hax3/acceptance/steps/Hooks.java
@@ -1,8 +1,8 @@
 package me.hax3.acceptance.steps;
 
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
 import io.cucumber.core.api.Scenario;
-import io.cucumber.java.After;
-import io.cucumber.java.Before;
 import me.hax3.ITCucumber;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
